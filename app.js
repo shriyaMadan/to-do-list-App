cf1e9dbc3e37068bc50f77a2ca7e805a5afec0d8
Shriya Madan <shriyamadan333@gmail.com>
2020-04-29 22:28:32 +0530

    changed the path of todo

--- app.js
+++ app.js
@@ -3,20 +3,14 @@ var app = express()
 const path = require("path")
 var bodyParser = require('body-parser')
 
-app.get('/', (req, res) => res.send('app serving at /todo'))
-
 app.use(express.static('public'))
 
 app.set("view engine" , "ejs")
   app.set('views',path.join(__dirname,'views'))
 
-app.get('/todo',function(_req, res){
+app.get('/',function(_req, res){
   res.render('index');
 });
-
-
-
-
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`app listening at http://localhost:${port}`))
