2e2793b764763d8574a8ca7bf8179f530b79e0b7
Shriya Madan <shriyamadan333@gmail.com>
2020-04-29 22:16:48 +0530

    FIX port number

--- app.js
+++ app.js
@@ -18,5 +18,5 @@ app.get('/todo',function(_req, res){
 
 
 
-const port = 3000
+const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`app listening at http://localhost:${port}`))
