b7dfacd84eb09adce0185ec67a409622baa2bed5
Shriya Madan <shriyamadan333@gmail.com>
2020-04-30 00:28:16 +0530

    ADD sessionsn

--- app.js
+++ app.js
@@ -1,28 +1,47 @@
 const express = require('express')
+const expressip = require('express-ip')
 var app = express()
 const path = require("path")
 var bodyParser = require('body-parser')
-const items = []
+var session = require('express-session')
+const { uuid } = require('uuidv4');
+
 app.use(express.static('public'))
-
+const data = []
+var sess = {
+  secret: 'keyboard cat',
+  cookie: {}
+}
+app.use(session(sess))
+app.use(expressip().getIpInfoMiddleware);
 app.set("view engine" , "ejs")
-  app.set('views',path.join(__dirname,'views'))
+app.set('views',path.join(__dirname, 'views'))
 
-app.get('/',function(_req, res){
-  res.render('index', {items});
+app.get('/',function(req, res){
+  console.log(req.ipInfo)
+  if (req.session.id == null) req.session.id = uuid()
+  res.render('index', {items: req.session.list});
 });
 
 app.get('/add', (req, res) => {
-  items.push(req.query.item)
-  res.send(items)
+  if (req.session.list == null) req.session.list = []
+  const id = req.session.id
+  const value = req.query.item
+  req.session.list.push(value)
+  data.push({id, value, ip: req.ipInfo})
+  res.send(req.session.list)
 })
 
 app.get('/delete', (req, res) => {
-  const index = items.indexOf(req.query.item);
+  const index = req.session.list.indexOf(req.query.item);
   if (index > -1) {
-    items.splice(index, 1);
+    req.session.list.splice(index, 1);
   }
-  res.send(items)
+  res.send(req.session.list)
+})
+
+app.get('/showme', (req, res) => {
+  res.send(data)
 })
 
 const port = process.env.PORT || 3000
